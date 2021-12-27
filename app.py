7aa66ed6c95ffcdbed1c5fdc12abe264530d66d7
Mohamed Aziz Ben Nessir <70135168+Mohamed-Aziz-Ben-Nessir@users.noreply.github.com>
2021-12-27 21:13:28 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse,FileResponse
+path="/home/mohamedaziz/port/posts/"
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get('/',response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get('/blog/{file}')
+def read(file):
+    file_path = os.path.join(path,file)
+    if os.path.exists(file_path):
+        return FileResponse(file_path,media_type="text/plain")
+    return {"error" : "File not found!"}
+@app.get('/blog', response_class=HTMLResponse)
+def Blog(request: Request):
+    files=list_files()
+    return templates.TemplateResponse("blog.html", {"request": request,"files":files})
+
+
+
+def list_files():
+    files = []
+    for filename in os.listdir("/home/mohamedaziz/port/posts"):
+        files.append(filename)
+    return files
+
+if __name__ == '__main__':
+    uvicorn.run(app) 
---
+++ posts/post1.md
@@ -0,0 +1,2 @@
+# Markdown: Syntax
+#	post1
