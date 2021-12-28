3643d1ecbee294c339c28a11f66c70877e05184a
Mohamed-Aziz-Ben-Nessir <mouhamedazizbennessir02@gmail.com>
2021-12-28 18:34:57 +0000

    5

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse,FileResponse
-path="/home/mohamedaziz/port/posts/"
+path="./posts/"
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
 
 @app.get('/',response_class=HTMLResponse)
 def index(request: Request):
@@ -21,16 +20,15 @@ def read(file):
     if os.path.exists(file_path):
         return FileResponse(file_path,media_type="text/plain")
     return {"error" : "File not found!"}
+
 @app.get('/blog', response_class=HTMLResponse)
 def Blog(request: Request):
     files=list_files()
     return templates.TemplateResponse("blog.html", {"request": request,"files":files})
 
-
-
 def list_files():
     files = []
-    for filename in os.listdir("/home/mohamedaziz/port/posts"):
+    for filename in os.listdir(path):
         files.append(filename)
     return files
 
